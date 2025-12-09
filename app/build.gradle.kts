39854da77645b099ab2e9372a5e09ba6333da60c
x0mar1b <115938398+omaribrahimmnasrr@users.noreply.github.com>
2025-12-09 19:14:53 +0200

    Task view model is done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 }
 
 dependencies {
-
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
